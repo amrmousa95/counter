574e2d05022ba71ea6431596b97601a86254f3e5
amrmousa95 <55198936+amrmousa95@users.noreply.github.com>
2022-11-20 22:06:02 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let board = document.getElementById('board')
+let reset = document.getElementById('reset')
+let counter = document.getElementById('counter')
+
+function rreset(){
+    board.innerHTML = 0
+}
+
+function Add(){
+    board.innerHTML ++
+}
